81a13d420ae2c941dc0bbd0ab6b533b32ff84f6b
Rafael Quintero <rafaqtro@gmail.com>
2023-09-08 12:18:16 -0400

    match_curr(), edit-show

--- bcv-exchange.py
+++ bcv-exchange.py
@@ -12,13 +12,29 @@ except:
 
 text = content.read()
 text_decode = text.decode('utf-8')
-usd_posn = text_decode.find('<span> USD</span>	 </div>')
 
-rest_of_text = text_decode[usd_posn:]
+#get date
+date_posn = text_decode.find('Fecha Valor:')
+rest_of_text = text_decode[date_posn:]
+date = re.search(r'<span \w+=".+">(\w+, \d+ \w+  \d+)</span>', rest_of_text)
 
-usd = re.search(r'<strong> (\d+,\d+) </strong>', rest_of_text)
+def match_curr(currency, text):
+    if currency == "EUR" or currency == "CNY":
+        posn = text.find('<span> '+currency+' </span>	 </div>')
+        rest_text = text[posn:]
+        curr_val = re.search(r'<strong> (\d+,\d+) </strong>', rest_text)
+    elif currency == "TRY" or currency == "RUB" or currency == "USD":
+        posn = text.find('<span> '+currency+' </span>	 </div>')
+        rest_text = text[posn:]
+        curr_val = re.search(r'<strong> (\d+,\d+) </strong>', rest_text)
+    return curr_val
 
-date = re.search(r'Fecha Valor: <span \w+=".+">(\w+, \d+ \w+  \d+)</span>', rest_of_text)
+test_euro = match_curr('EUR', text_decode)
+test_cny  = match_curr('CNY', text_decode)
+test_try  = match_curr('TRY', text_decode)
+test_rub  = match_curr('RUB', text_decode)
+test_usd  = match_curr('USD', text_decode)
+
 
 def get_value(re_match):
     if re_match:
@@ -27,22 +43,30 @@ def get_value(re_match):
         value = "can't get value"
     return value
 
-def show(u, d):
+def show(u, d, cs):
     print("---------------------------")
-    print(f"$ BCV = {u} ")
+    print(f"{cs} BCV = {u} ")
     print(f"{d}")
     print("---------------------------")
 
-usd_v = get_value(usd)
+eur_v = get_value(test_euro)
+cny_v = get_value(test_cny)
+try_v = get_value(test_try)
+rub_v = get_value(test_rub)
+usd_v = get_value(test_usd)
 date_v = get_value(date)
 
 #change the form nn,nn to nn.nn
 # ex 33,33 to 33.33
-usd_v2 = usd_v.replace(",",".")
-usd_float = float(usd_v2)
-usd_round = round(usd_float, 4)
+#usd_v2 = usd_v.replace(",",".")
+#usd_float = float(usd_v2)
+#usd_round = round(usd_float, 4)
  
-show(usd_round, date_v)
+show(eur_v, date_v,"€")
+show(cny_v, date_v,"¥")
+show(try_v, date_v,"₺")
+show(rub_v, date_v,"₽")
+show(usd_v, date_v,"$")
 
 
 
